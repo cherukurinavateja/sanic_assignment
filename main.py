b6e57d12ef8a7d6601301d41eb29c84b3b18f317
navateja cherukuri <navateja@navatejas-Air.lan>
2022-02-25 22:30:57 -0600

    new

--- main.py
+++ main.py
@@ -80,4 +80,4 @@ async def delete_data(request):
     return json(response, status=status_code)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
